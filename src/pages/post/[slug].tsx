22913ee2eae72c3565486bd67351a43142546d47
Lucas Lopes <dev.thuram@gmail.com>
2021-04-08 07:47:24 -0300

    finish post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -28,9 +29,46 @@ interface PostProps {
 
 export default function Post(): JSX.Element {
   return (
-    <div className={commonStyles.container}>
-      <h1>Aqui é o post</h1>
-    </div>
+    <>
+      <img
+        src="https://raw.githubusercontent.com/octref/polacode/master/demo/1.png"
+        alt="titulo do post"
+        className={styles.banner}
+      />
+      <div className={commonStyles.container}>
+        <div className={styles.postInformationsContainer}>
+          <h1>Criando um app do zero</h1>
+
+          <ul className={commonStyles.postInformations}>
+            <li>
+              <FiCalendar />
+              <time>19 Abr 2021</time>
+            </li>
+            <li>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </li>
+            <li>
+              <FiClock />
+              <span>4 min</span>
+            </li>
+          </ul>
+          <span className={styles.editedAt}>
+            * editado em 19 mar 2021, às 15:49
+          </span>
+        </div>
+
+        <article>
+          <h2>Lorem ipsum</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod autem
+            dolorum fuga similique alias, impedit est minima, sed, eum eius
+            blanditiis iusto voluptatibus vitae soluta praesentium amet
+            consequuntur at eligendi.
+          </p>
+        </article>
+      </div>
+    </>
   );
 }
 
